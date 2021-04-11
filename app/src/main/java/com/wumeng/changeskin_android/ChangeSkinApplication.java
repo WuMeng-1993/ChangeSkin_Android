eea9d72abb21671e0a82bde6eec35dbe17e9b4c7
WuMeng <541345568@qq.com>
2021-04-11 23:46:07 +0800

    创建仓库

---
+++ app/src/main/java/com/wumeng/changeskin_android/ChangeSkinApplication.java
@@ -0,0 +1,18 @@
+package com.wumeng.changeskin_android;
+
+import android.app.Application;
+
+/**
+ * @author WuMeng
+ * @date 2021/4/11
+ * desc:
+ */
+class ChangeSkinApplication extends Application {
+
+    
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
